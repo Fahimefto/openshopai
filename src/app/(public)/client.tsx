2353d5bb82e6538263ca0e44c2a104b79494a88a
Fahim Iftekhar Efto <ifto.141.fi@gmail.com>
2024-05-26 17:08:14 +0600

    add toast fix label

--- src/app/(public)/client.tsx
+++ src/app/(public)/client.tsx
@@ -1,19 +1,72 @@
-import MainLayout from "@/components/layout/main-layout";
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  url: z.string().url({ message: "URL Required" }),
+});
 
 const InputURL = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      url: "",
+    },
+  });
+  const { toast } = useToast();
+  const router = useRouter();
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    toast({
+      title: "Done!",
+      description: "We are analyzing your website and extracting data.",
+    });
+    router.push("/import");
+  }
   return (
-    <div className="w-full max-w-lg items-center mx-auto pt-5">
-      <form className="flex gap-2">
-        <Input
-          type="email"
-          placeholder="Enter your shop URL"
-          className="border-primary"
-        />
-        <Button type="submit">Get Started!- Its Free</Button>
-      </form>
+    <div className="w-full pt-5 ">
+      <Form {...form}>
+        <form
+          className="flex gap-2 justify-center max-w-xl mx-auto"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="">
+                  <Input
+                    placeholder="Enter your shop URL"
+                    className="border-primary min-w-full"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">Get Started!- Its Free</Button>
+        </form>
+      </Form>
       <h3 className="text-center pt-5 text-primary/50 font-bold text-sm">
         * Get your chatbot in miniutes *
       </h3>
